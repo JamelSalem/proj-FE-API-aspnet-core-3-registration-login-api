ab33c3f5544bf7d3f32da5ec9146cb3262bd497d
SalemJamel <SalemJamalfsg@gmail.com>
2021-02-11 02:10:40 +0100

    dd

--- Helpers/AppException.cs
+++ Helpers/AppException.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace WebApi.Helpers
 {
-    // Custom exception class for throwing application specific exceptions (e.g. for validation) 
+    // Custom hhhhh exception class for throwing application specific exceptions (e.g. for validation) 
     // that can be caught and handled within the application
     public class AppException : Exception
     {
